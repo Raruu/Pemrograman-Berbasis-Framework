1c52507daddb5a024cffc1aa31e301f263ed2aef
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2026-04-10 14:00:07 +0700

    JS13 complete

--- P09/js13/src/pages/produk/index.tsx
+++ P09/js13/src/pages/produk/index.tsx
@@ -1,13 +1,14 @@
+import fetcher from "@/utlis/swr/fetcher";
+import TampilanProduk from "@/views/produk";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import TampilanProduk from "../../views/produk";
 import useSWR from "swr";
-import fetcher from "../../utlis/swr/fetcher";
 
 // const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 const Kategori = () => {
-  // const [isLogin, setIsLogin] = useState(false);
-  // const { push } = useRouter();
+  // const [isLogin, setIsLogin] = useState(true);
+  const { push } = useRouter();
   const [products, setProducts] = useState([]);
   // console.log("products:", products);
 
