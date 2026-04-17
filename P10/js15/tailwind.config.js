95d77e355e28c891245dd1e7be1763b489e87cfe
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2026-04-17 12:27:03 +0700

    init JS15 cp FROM JS14

---
+++ P10/js15/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
