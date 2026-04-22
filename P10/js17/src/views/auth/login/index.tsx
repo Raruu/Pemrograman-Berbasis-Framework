7a8db549fb9dd28d250b4c8ece99501c27debbd5
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2026-04-22 19:41:23 +0700

    init JS17 cp from JS16

---
+++ P10/js17/src/views/auth/login/index.tsx
@@ -0,0 +1,97 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Link from "next/link";
+import style from "./login.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+
+const TampilanLogin = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { push, query } = useRouter();
+  const callbackUrl: any = query.callbackUrl || "/";
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    setError("");
+    setIsLoading(true);
+
+    //const form = event.currentTarget; ...
+    // }
+    try {
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: event.target.email.value,
+        password: event.target.password.value,
+        callbackUrl,
+      });
+
+      // console.log("signIn response:", res);
+      if (!res?.error) {
+        setIsLoading(false);
+        push(callbackUrl);
+      } else {
+        setIsLoading(false);
+        setError(res?.error || "Login failed");
+      }
+    } catch (error) {
+      setIsLoading(false);
+      setError("wrong email or password");
+    }
+  };
+
+  return (
+    <>
+      <div className={style.login}>
+        {error && <p className={style.login__error}>{error}</p>}
+        <h1 className={style.login__title}>Halaman Login</h1>
+        <div className={style.login__form}>
+          <form onSubmit={handleSubmit}>
+            <div className={style.login__form__item}>
+              <label htmlFor="email" className={style.login__form__item__label}>
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                className={style.login__form__item__input}
+              />
+            </div>
+
+            <div className={style.login__form__item}>
+              <label
+                htmlFor="Password"
+                className={style.login__form__item__label}
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="Password"
+                name="password"
+                placeholder="Password"
+                className={style.login__form__item__input}
+              />
+            </div>
+            <button
+              type="submit"
+              className={style.login__form__item__button}
+              disabled={isLoading}
+            >
+              {isLoading ? "Memproses..." : "Login"}
+            </button>
+          </form>
+          <br />
+          <p className={style.login__form__item__text}>
+            Tidak punya akun?{" "}
+            <Link href="/auth/register">Ke Halaman Register</Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TampilanLogin;
